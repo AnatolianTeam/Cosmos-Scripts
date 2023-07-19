a8fffdbbe22daa5e4cafde9d82ea254bbc7e117a
Mehmet Köl Tiğin <102043225+koltigin@users.noreply.github.com>
2023-07-19 18:55:51 +0300

    Update v12.1.0.sh

--- stride/stride-1/upgrade/v12.1.0.sh
+++ stride/stride-1/upgrade/v12.1.0.sh
@@ -5,4 +5,5 @@ make install
 strided version
 #commit: bbf0bb7f52878f3205c76bb1e96662fe7bd7af8d
 #version: v12.1.0
+wget -O $HOME/.stride/config/ccv.json "https://raw.githubusercontent.com/Stride-Labs/mainnet/main/ics-instructions/ccv.json"
 systemctl restart strided
