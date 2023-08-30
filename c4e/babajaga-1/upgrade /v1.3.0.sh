09590e6f4fbc8f54c838c6a41ef5a8a2c0044555
Mehmet Köl Tiğin <102043225+koltigin@users.noreply.github.com>
2023-08-30 12:39:18 +0300

    Update v1.3.0.sh

--- c4e/babajaga-1/upgrade /v1.3.0.sh
+++ c4e/babajaga-1/upgrade /v1.3.0.sh
@@ -1,7 +1,12 @@
 systemctl stop c4ed
-cd || return
+cd $HOME
 rm -rf c4e-chain
-git clone --depth 1 --branch  v1.3.0  https://github.com/chain4energy/c4e-chain.git
+git clone https://github.com/chain4energy/c4e-chain.git
 cd c4e-chain
+git fetch --tags
+git checkout v1.3.0
 make install
-systemctl start c4ed
+c4ed version
+mv build/c4ed $(which c4ed)
+mv ~/go/bin/c4ed /usr/local/bin/c4ed
+systemctl restart c4ed
